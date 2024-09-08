38db3fb698113ee1517435ed4777484310adb4e8
Aaron Villalpando <aaron@boundaryml.com>
2024-09-08 11:25:14 -0700

    polyfill

--- myapp/next.config.mjs
+++ myapp/next.config.mjs
@@ -31,4 +31,26 @@ const nextConfig = {
   },
 };
 
+if (typeof Promise.withResolvers === "undefined") {
+  if (typeof window !== "undefined") {
+    window.Promise.withResolvers = function () {
+      let resolve, reject;
+      const promise = new Promise((res, rej) => {
+        resolve = res;
+        reject = rej;
+      });
+      return { promise, resolve, reject };
+    };
+  } else {
+    global.Promise.withResolvers = function () {
+      let resolve, reject;
+      const promise = new Promise((res, rej) => {
+        resolve = res;
+        reject = rej;
+      });
+      return { promise, resolve, reject };
+    };
+  }
+}
+
 export default nextConfig;

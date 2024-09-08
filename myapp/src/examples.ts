3a5e4e3cfc176c3eed6b0ae25973be2256df750a
Aaron Villalpando <aaron@boundaryml.com>
2024-09-08 15:33:20 -0700

    thing

--- myapp/src/examples.ts
+++ myapp/src/examples.ts
@@ -2,6 +2,7 @@ const exampleFiles = [
     // { name: 'cursor_rules.jpeg', type: 'image/jpeg', url: '/examples/cursor_rules.jpeg' },
     // { name: 'download.png', type: 'image/png', url: '/examples/download.png' },
     { name: 'Luna.png', type: 'image/png', url: '/examples/Luna.png' },
+    { name: 'us.jpg', type: 'image/jpg', url: '/examples/us.jpg' },
     { name: 'invoice.png', type: 'image/png', url: '/examples/invoice.png' },
     { name: 'cambodia-visa.jpg', type: 'image/jpg', url: '/examples/cambodia-visa.jpg' },
     { name: 'ai-conference-floor-plan.png', type: 'image/png', url: '/examples/ai-conference-floor-plan.png' },

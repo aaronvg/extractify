d13b70b0318afff53479ba8d9e8e75dddfb208a0
Aaron Villalpando <aaron@boundaryml.com>
2024-09-07 22:39:51 -0700

    add baml nextjs config

--- myapp/next.config.mjs
+++ myapp/next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@boundaryml/baml"],
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
+  webpack: (config, { dev, isServer, webpack, nextRuntime }) => {
+    config.module.rules.push({
+      test: /\.node$/,
+      use: [
+        {
+          loader: "nextjs-node-loader",
+          options: {
+            outputPath: config.output.path,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
